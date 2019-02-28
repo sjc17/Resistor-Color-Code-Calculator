d0b62043689bf64ddfd292a7a82754efaa6c8d0e
Steven Choi <sjc@ualberta.ca>
2019-02-28 13:34:56 -0700

    Dynamic resistor band color change with text input

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ class App extends Component {
     };
     this.textChangeCallback = this.textChangeCallback.bind(this);
     this.handleBandCountChange = this.handleBandCountChange.bind(this);
+    this.handleBandSelect = this.handleBandSelect.bind(this);
   }
   // Update when user inputs numbers or SI prefixes in text box
   textChangeCallback(textData) {
@@ -25,6 +26,9 @@ class App extends Component {
   // Update when user selects band count from dragdown box
   handleBandCountChange(event) {
     this.setState({ numOfBands: event.target.value, currentBand: 0 });
+  }
+  handleBandSelect(event) {
+    this.setState({ currentBand: event.target.value });
   }
   render() {
     return (
@@ -46,10 +50,10 @@ class App extends Component {
             <option>3</option>
             <option selected>4</option>
             <option>5</option>
-            <option>6</option>
           </select>
         </div>
         <ColorPicker
+          onSelect={this.handleBandSelect}
           currentBand={this.state.currentBand}
           value={this.state.resistance}
         />
@@ -95,10 +99,102 @@ class TextField extends Component {
 
 // Render resistor display and enable selecting bands
 class ResistorDisplay extends Component {
+  getMultiplier(number) {
+    // Two digit bands
+    if (this.props.numOfBands <= 4) {
+      if (number >= Math.pow(10, 8)) return 7;
+      else if (number >= Math.pow(10, 7)) return 6;
+      else if (number >= Math.pow(10, 6)) return 5;
+      else if (number >= Math.pow(10, 5)) return 4;
+      else if (number >= Math.pow(10, 4)) return 3;
+      else if (number >= Math.pow(10, 3)) return 2;
+      else if (number >= Math.pow(10, 2)) return 1;
+      else if (number >= Math.pow(10, 1)) return 0;
+      else if (number >= 1) return 0.1;
+      else if (number > 0) return 0.01;
+      else return 0;
+    }
+    // Three digit bands
+    else {
+      if (number >= Math.pow(10, 9)) return 7;
+      else if (number >= Math.pow(10, 8)) return 6;
+      else if (number >= Math.pow(10, 7)) return 5;
+      else if (number >= Math.pow(10, 6)) return 4;
+      else if (number >= Math.pow(10, 5)) return 3;
+      else if (number >= Math.pow(10, 4)) return 2;
+      else if (number >= Math.pow(10, 3)) return 1;
+      else if (number >= Math.pow(10, 2)) return 0;
+      else if (number >= Math.pow(10, 1)) return 0.1;
+      else if (number > 0) return 0.01;
+      else return 0;
+    }
+  }
+  getFirstDigit(number) {
+    const multiplier = this.getMultiplier(number);
+    let index;
+    if (number === 0) return 0;
+    if (this.props.numOfBands <= 4) {
+      if (multiplier != 0.01) index = 0;
+      else if (number >= 1) index = 0;
+      else if (number > 0) index = 2;
+      else return 0;
+    } else {
+      index = 0;
+    }
+    return parseInt(number.toString().substr(index, 1));
+  }
+
+  getSecondDigit(number) {
+    const multiplier = this.getMultiplier(number);
+    let index;
+    // Two digit bands
+    if (this.props.numOfBands <= 4) {
+      if (multiplier != 0.1 && multiplier != 0.01) {
+        if (number >= 100) index = 2;
+        else index = 1;
+      } else if (multiplier === 0.1) index = 2;
+      else if (multiplier === 0.01) index = 3;
+      else return 0;
+    }
+    // Three digit bands
+    else {
+      if (multiplier != 0.1 && multiplier != 0.01) {
+        if (number >= 1000) index = 2;
+        else index = 1;
+      } else if (multiplier === 0.1) {
+        index = 1;
+      } else index = 2;
+    }
+    return parseInt(number.toString().substr(index, 1));
+  }
+  getThirdDigit(number) {
+    const multiplier = this.getMultiplier(number);
+    let index;
+    // Three digit bands
+    if (number >= 1000) index = 3;
+    else if (multiplier != 0.1 && multiplier != 0.01) index = 2;
+    else if (multiplier === 0.1) index = 3;
+    else if (number > 0) index = 3;
+    else return 0;
+    return parseInt(number.toString().substr(index, 1));
+  }
   render() {
     // Code to remove whitespace is repeated here to fix a bug with this component showing whitespace anyways
     const resistanceString = this.props.value.replace(/\s/g, "");
-    let actualResistance = parseSIPrefix(resistanceString);
+    let actualResistance = 0;
+    if (resistanceString.match(validUserInputRegEx)) {
+      actualResistance = parseFloat(
+        parseSIPrefix(resistanceString).toFixed(2)
+      ).toPrecision(this.props.numOfBands <= 4 ? 2 : 3);
+      if (
+        this.props.numOfBands >= 5 &&
+        (actualResistance >= 0.01 && actualResistance <= 9990 * Math.pow(10, 6))
+      ) {
+      } else {
+        //actualResistance = 0;
+        //alert("Not valid number");
+      }
+    }
     return (
       <div className="ResistorDisplayBox">
         <div className="DisplayResistanceValue">
@@ -109,12 +205,21 @@ class ResistorDisplay extends Component {
           <div className="wire wire-left" />
           <div className="wire wire-right" />
           {/* Digit 1 */}
-          <div className="band band1" style={bandColor(0, 2)} />
+          <div
+            className="band band1"
+            style={bandColor(0, this.getFirstDigit(actualResistance))}
+          />
           {/* Digit 2 */}
-          <div className="band band2" style={bandColor(1, 0)} />
+          <div
+            className="band band2"
+            style={bandColor(1, this.getSecondDigit(actualResistance))}
+          />
           {/* Multiplier */}
           {this.props.numOfBands >= 3 ? (
-            <div className="band band3" style={bandColor(2, 0)} />
+            <div
+              className="band band3"
+              style={bandColor(2, this.getMultiplier(actualResistance))}
+            />
           ) : null}
           {/* Tolerance */}
           {this.props.numOfBands >= 4 ? (
@@ -122,11 +227,10 @@ class ResistorDisplay extends Component {
           ) : null}
           {/* Digit 3 */}
           {this.props.numOfBands >= 5 ? (
-            <div className="band band5" style={bandColor(4, 6)} />
-          ) : null}
-          {/* Temperature Coefficient */}
-          {this.props.numOfBands >= 6 ? (
-            <div className="band band6" style={bandColor(5, 100)} />
+            <div
+              className="band band5"
+              style={bandColor(4, this.getThirdDigit(actualResistance))}
+            />
           ) : null}
         </div>
       </div>
@@ -139,12 +243,11 @@ class ColorPicker extends Component {
   constructor(props) {
     super(props);
     this.bandString = [
-      "Digit 1",
-      "Digit 2",
-      "Multiply By",
-      "Tolerance",
-      "Digit 3",
-      "Temperature Coefficient (ppm/K)"
+      "Digit 1", // 0
+      "Digit 2", // 1
+      "Multiply By", // 2
+      "Tolerance", // 3
+      "Digit 3" // 4
     ];
   }
   render() {
@@ -175,7 +278,6 @@ function bandColor(bandNum, value) {
   switch (bandNum) {
     case 0:
     case 1:
-    case 2:
     case 4:
       // Digit band colors
       switch (value) {
@@ -210,6 +312,44 @@ function bandColor(bandNum, value) {
           color = "white";
           break;
         default:
+          color = "black";
+          break;
+      }
+      break;
+    // Multiplier band colors
+    case 2:
+      switch (value) {
+        case 0:
+          color = "black";
+          break;
+        case 1:
+          color = "brown";
+          break;
+        case 2:
+          color = "red";
+          break;
+        case 3:
+          color = "orange";
+          break;
+        case 4:
+          color = "yellow";
+          break;
+        case 5:
+          color = "green";
+          break;
+        case 6:
+          color = "blue";
+          break;
+        case 7:
+          color = "violet";
+          break;
+        case 0.1:
+          color = "gold";
+          break;
+        case 0.01:
+          color = "silver";
+          break;
+        default:
           break;
       }
       break;
@@ -236,25 +376,6 @@ function bandColor(bandNum, value) {
           break;
         case 0.1:
           color = "violet";
-          break;
-        default:
-          break;
-      }
-      break;
-    // Temperature coefficient
-    case 5:
-      switch (value) {
-        case 100:
-          color = "brown";
-          break;
-        case 50:
-          color = "red";
-          break;
-        case 15:
-          color = "orange";
-          break;
-        case 25:
-          color = "yellow";
           break;
         default:
           break;
